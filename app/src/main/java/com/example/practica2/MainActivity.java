4ddbca4ed828945e886d8ebe6cfde437645bd59d
aracavic <aracamatelmo@gmail.com>
2024-10-11 15:09:15 +0200

    Hola que tal

--- app/src/main/java/com/example/practica2/MainActivity.java
+++ app/src/main/java/com/example/practica2/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Holakease
+        //HOla que tal
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
