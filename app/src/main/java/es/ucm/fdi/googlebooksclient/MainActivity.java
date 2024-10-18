d5e85a393ef9aff01a07367f39fceb85155a2d21
aracavic <aracamatelmo@gmail.com>
2024-10-18 15:31:37 +0200

    Varios cambios en diseño y funcionalidad del main.

--- app/src/main/java/es/ucm/fdi/googlebooksclient/MainActivity.java
+++ app/src/main/java/es/ucm/fdi/googlebooksclient/MainActivity.java
@@ -1,6 +1,9 @@
 package es.ucm.fdi.googlebooksclient;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etAutor;
+    private RadioButton rbLibro, rbRevista;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Referencias a los elementos del layout
+        etAutor = findViewById(R.id.etAutor);
+        rbLibro = findViewById(R.id.rbLibro);
+        rbRevista = findViewById(R.id.rbRevista);
+
+        // Listener para el RadioGroup
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rbRevista) {
+                    etAutor.setEnabled(false);  // Desactiva el campo autor
+                    etAutor.setText("");  // Limpia el campo si hay texto
+                } else if (checkedId == R.id.rbLibro) {
+                    etAutor.setEnabled(true);  // Activa el campo autor
+                }
+            }
         });
     }
 }
