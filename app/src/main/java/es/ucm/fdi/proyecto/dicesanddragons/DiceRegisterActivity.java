26b3d5035714d9f262c434cc9edf0c209b457c2d
aracavic <aracamatelmo@gmail.com>
2024-11-22 15:32:19 +0100

    tiradas de dados y cambios en creador de armas

---
+++ app/src/main/java/es/ucm/fdi/proyecto/dicesanddragons/DiceRegisterActivity.java
@@ -0,0 +1,15 @@
+package es.ucm.fdi.proyecto.dicesanddragons;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DiceRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dice_register_activity);
+
+    }
+}
