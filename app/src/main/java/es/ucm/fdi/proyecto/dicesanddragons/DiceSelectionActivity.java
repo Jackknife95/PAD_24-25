38cac532aeecbae9cda7193d8b61dafdd070cfa1
DiegoMontero747 <diemon01@ucm.es>
2024-11-22 15:04:02 +0100

    actividad de seleccion de tiradas

---
+++ app/src/main/java/es/ucm/fdi/proyecto/dicesanddragons/DiceSelectionActivity.java
@@ -0,0 +1,20 @@
+package es.ucm.fdi.proyecto.dicesanddragons;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DiceSelectionActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dice_roll_activity);
+        // Aquí se añaden las funcionalidades de tiradas de dados
+
+
+
+    }
+}
+
+
